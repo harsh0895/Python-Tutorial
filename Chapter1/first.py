8d15e3bd40d77fff6d62e7f6ff88bbb6c15f59c0
harsh <hb755037@gmail.com>
2024-07-25 15:41:24 +0530

    third

---
+++ Chapter1/first.py
@@ -0,0 +1,28 @@
+# INTRODUCTION TO PYTHON:-
+
+# First program:-
+print("Hello world!")
+print("I am learning python!")
+
+
+# Variables in python:-
+
+name = "Harsh"
+age = 21
+price = 25.99
+
+print(name," ", age, " ", price)
+print(type(name))
+print(type(age))
+print(type(price))
+
+
+# Expression execution:-
+a = 2
+b = 3
+txt = "@"
+print(a*txt*b)
+
+
+ 
+
---
+++ Chapter1/input.py
@@ -0,0 +1,20 @@
+# input in python:-
+
+# name = input("Enter your name: ")
+# age = int(input("enter your age: "))
+# price = float(input("Enter your price: "))
+
+# print("Name is: ",name)
+# print(age)
+# price(price)
+
+# Contional statements in python:-
+
+Age = int(input("enter your age: "))
+
+if( Age > 18 ): 
+    print("You can drive")
+elif( Age <= 18 ):
+    print("You cannot drive")
+else:
+    print("you can go!")
